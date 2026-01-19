30a21ece9de2d4b02dda414c74ecc07cc1b1e3e3
Ianluck Rop <ianluckrp@ciencias.unam.mx>
2026-01-18 20:40:39 -0600

    First attempt with tauri-rust connection

--- app_pm/src-tauri/src/commands.rs
+++ app_pm/src-tauri/src/commands.rs
@@ -1,47 +1,105 @@
 use core_pm::password_manager::PasswordManager;
+use core_pm::models::{EntryView, NewEntry, UpdateEntry, SearchField}; 
+
+#[derive(Debug, thiserror::Error)]
+pub enum ErrPM {
+    #[error("No database is open")]
+    NoDatabase,
+
+    #[error("I/O error: {0}")]
+    Io(String),
+
+    #[error("Database error: {0}")]
+    Db(String),
+}
+
+
+// we must manually implement serde::Serialize
+impl serde::Serialize for ErrPM {
+  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+  where
+    S: serde::ser::Serializer,
+  {
+    serializer.serialize_str(self.to_string().as_ref())
+  }
+}
+
+let mut manager;
 
 #[tauri::command]
-pub fn init_manager() {
+pub fn init_manager() Result<(), ErrPM>{
+    manager = PasswordManager::new()?;
+    Ok(())
 }
 
-/*
-#[tauri::command(rename_all = "snake_case")]
-pub fn open_db_request(path: &str,
-		       key: &str) {
+#[tauri::command]
+pub fn open_db_request(dir: String,
+		       key: String) -> Result<(), ErrPM>{
+    let path  = dir.path();
+    let secret = SecretString::new(key);
+    manager.open_db(&path, &secret);
+    Ok(())
 }
 
-#[tauri::command(rename_all = "snake_case")]
-pub fn create_db_request(path: &str,
-			 key: &str) {
-}
-
-#[tauri::command(rename_all = "snake_case")]
-pub fn new_password(path: &str,
-		    key: &str) {
-}
-
-#[tauri::command(rename_all = "snake_case")]
-pub fn edit_password(path: &str,
-		     key: &str) {
-}
-
-#[tauri::command(rename_all = "snake_case")]
-pub fn show_all_passwords(path: &str,
-			  key: &) {
+#[tauri::command]
+pub fn create_db_request(dir: &str,
+			 key: String) -> Result<(), ErrPM>{
+    let path  = dir.path();
+    let secret = SecretString::new(key);
+    manager.create_db(&path, &secret);
+    Ok(())
 }
 
 #[tauri::command(rename_all = "snake_case")]
-pub fn search_passwords(path: &str,
-			key: &str) {
+pub fn new_row(service_data: String,
+	       email_data: String,
+	       username_data: String,
+	       password_data: String) Result<(), ErrPM> {
+    let row =
+	NewEntry {
+	    service: service_data,
+	    email: Some(email_data),
+	    username: Some(username_data),
+	    password: SecretString::new(password_data)
+	};
+    manager.new_entry(&row)?;
 }
 
 #[tauri::command(rename_all = "snake_case")]
-pub fn reveal_hidden_password(path: &str,
-			      key: &str) {
+pub fn edit_row(row_id,
+		row_service: String,
+		row_email: String,
+		row_username: String,
+		row_password: String) -> Result<(), ErrPM> {
+    let update_row = UpdateEntry {
+	id: row_id,
+	service: row_service,
+	email: row_email,
+	username: row_username,
+	password: SecreString::new(row_password)
+    };
+
+    manager.edit_entry(&update_row);
 }
 
 #[tauri::command(rename_all = "snake_case")]
-pub fn copy_to_clipboard_password(path: &str,
-				  key: &str) {
+pub fn delete_row(row_id: i64) -> Result<(), ErrPM> {
+    manager.remove_entry(row_id)?;
+    Ok()
 }
-*/
+
+#[tauri::command(rename_all = "snake_case")]
+pub fn password_request(row_id: i64) -> Result<String, ErrPM> {
+    let secret = manager.get_password(row_id)?;
+    Ok(secret.expose_secret().to_string())
+}
+
+#[tauri::command]
+pub fn show_all_rows() -> Result<Vec<EntryView>, String> {
+    let rows = show_all_entries()?;
+    Ok(rows)
+}
+
+#[tauri::command(rename_all = "snake_case")]
+pub fn search_rows() {
+}
--- app_pm/src/main.js
+++ app_pm/src/main.js
@@ -2,6 +2,9 @@ import { loadHome } from "/views/home_page/home.js";
 
 const { invoke } = window.__TAURI__.core;
 
+//const invoke = window.__TAURI__.core.invoke;
+//invoke('init_manager');
+
 window.addEventListener("DOMContentLoaded",
 			() => { loadHome(); }
 		       );
